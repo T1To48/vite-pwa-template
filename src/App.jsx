7cc77409f8b8636e4e16b8e4df63ab1e4355f88c
T1To48 <tofek.98@gmail.com>
2023-04-13 01:46:08 +0300

    react native, and vibration method for phones

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Vibration } from 'react-native';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +19,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {
+           Vibration.vibrate()
+          setCount((count) => count + 1)
+          }}>
           count is {count}
         </button>
         <p>
