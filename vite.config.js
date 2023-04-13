00709235de93865ce858cd189e70f54ee981f882
T1To48 <tofek.98@gmail.com>
2023-04-14 01:39:27 +0300

    registerSW

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { registerSW } from 'virtual:pwa-register'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,7 +40,8 @@ export default defineConfig({
         ]
       }
 
-     })
+     }),
+     registerSW({ immediate: true })
   ],
   server:{
     host:true
